7c99c4253cb388a17a2603e369438ceaa7d96e79
r.stngkr <151922812+rolandsitungkir@users.noreply.github.com>
2023-12-06 03:01:28 -0800

    Add files via upload

---
+++ R.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belajar PHP</title>
+</head>
+<body>
+<h1>Belajar PHP</h1>
+    <?php
+    $a=10;
+    $b=4;
+    $c=14;
+    $d=7;
+    $e=3;
+
+    echo "Hasil Dari Tambahan $a+$b=$c";
+    echo "</br>";
+    echo "=============================";
+    echo "</br>";
+    echo "Hasil dari pengurangan $d-$b=$e";
+</body>
+</html>
